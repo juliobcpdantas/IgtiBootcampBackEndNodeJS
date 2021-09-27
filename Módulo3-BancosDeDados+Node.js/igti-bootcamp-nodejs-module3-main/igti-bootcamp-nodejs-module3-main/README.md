889611c95754f995b200915389ab93188d1428ed
JULIOBCPDANTAS <85968604+juliobcpdantas@users.noreply.github.com>
2021-09-27 20:50:47 -0300

    Delete para teste
    Delete para teste

--- Módulo3-BancosDeDados+Node.js/igti-bootcamp-nodejs-module3-main/igti-bootcamp-nodejs-module3-main/README.md
+++
@@ -1,2 +0,0 @@
-# igti-bootcamp-nodejs-module3
-Code developed by teacher Guilherme Assis and used in the recorded classes of module 3 (Node.js + Databases) of the Bootcamp Node.js Developer from IGTI
--- Módulo3-BancosDeDados+Node.js/igti-bootcamp-nodejs-module3-main/igti-bootcamp-nodejs-module3-main/store-api - mongoose/repositories/mongo.db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-async function connect() {
-    const uri = "mongodb+srv://root:igti@cluster0.pi7nj.mongodb.net/store?retryWrites=true&w=majority";
-    return await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-}
-
-export { connect }
